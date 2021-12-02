99c40bb9801803a65ec04994037892eeb43e6b54
Brage Salhus Bunk <bragesb@gmail.com>
2021-12-02 08:46:58 +0100

    Day1 done

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Lib
+import Solutions
 
 main :: IO ()
-main = someFunc
+main = do i1 <- readFile "inputfiles/input1"
+          putStrLn $ solution1a i1
+          putStrLn $ solution1b i1
---
+++ src/Solutions.hs
@@ -0,0 +1,19 @@
+module Solutions
+    ( solution1a
+    , solution1b
+    ) where
+
+solution1a :: String -> String
+solution1a = show . solve1a . parse1
+
+solution1b :: String -> String
+solution1b = show . solve1b . parse1
+
+parse1 :: String -> [Integer]
+parse1 = map read . lines
+
+solve1a :: [Integer] -> Int
+solve1a is = length . filter (uncurry (<)) . zip is $ tail is
+
+solve1b :: [Integer] -> Int
+solve1b is = solve1a . zipWith3 (\a b c -> a + b + c) is (tail is) $ (tail . tail $ is)
